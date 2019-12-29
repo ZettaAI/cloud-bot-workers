210c0ac81f40d920cbfea3c5da23351b60038073
Akhilesh Halageri <akhileshhalageri@gmail.com>
2019-12-29 22:00:56 +0000

    reafctors

--- workers/__init__.py
+++ workers/__init__.py
@@ -3,9 +3,40 @@ Package for workers.
 The workers listen to messages in queues from an exchange.
 RabbitMQ is the messsage broker.
 """
+import os
+from collections import namedtuple
 
 from dotenv import load_dotenv
+from pika import BlockingConnection
+from pika import ConnectionParameters
+from pika.credentials import PlainCredentials
+
 
 load_dotenv()
 
-EXCHANGE_NAME = "cloud_bot"
+_config_fields = (
+    "SLACK_API_TOKEN",
+    "SLACK_SIGNING_SECRET",
+    "AMQP_SERVICE_HOST",
+    "AMQP_USERNAME",
+    "AMQP_PASSWORD",
+    "EXCHANGE_NAME",
+)
+_config_defaults = (
+    os.environ["SLACK_API_TOKEN"],
+    os.environ["SLACK_SIGNING_SECRET"],
+    os.environ.get("AMQP_SERVICE_HOST", "localhost"),
+    os.environ.get("AMQP_USERNAME", "guest"),
+    os.environ.get("AMQP_PASSWORD", "guest"),
+    os.environ.get("EXCHANGE_NAME", "cloud_bot"),
+)
+Config = namedtuple("Config", _config_fields, defaults=_config_defaults,)
+config = Config()
+
+amqp_cnxn = BlockingConnection(
+    ConnectionParameters(
+        host=config.AMQP_SERVICE_HOST,
+        credentials=PlainCredentials(config.AMQP_USERNAME, config.AMQP_PASSWORD,),
+    )
+)
+
--- workers/gcloud/create_bucket.py
+++ workers/gcloud/create_bucket.py
@@ -7,16 +7,8 @@ import slack
 from google.cloud import storage
 
 from . import ROUTING_KEY
-from .. import EXCHANGE_NAME
-
-
-config = Config("deploy.env")
-DEBUG = config("DEBUG", cast=bool, default=False)
-AMQP_CREDS = pika.credentials.PlainCredentials(
-    config.get("AMQP_USERNAME", default="guest"),
-    config.get("AMQP_PASSWORD", default="guest"),
-)
-AMQP_SERVICE_HOST = config.get("AMQP_SERVICE_HOST", default="localhost")
+from .. import config
+from .. import amqp_cnxn
 
 
 def callback(ch, method, properties, body):
@@ -30,14 +22,15 @@ def callback(ch, method, properties, body):
     # assert response["ok"]
 
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(host="172.17.0.4"))
-channel = connection.channel()
+channel = amqp_cnxn.channel()
 
-channel.exchange_declare(exchange=EXCHANGE_NAME, exchange_type="topic")
+channel.exchange_declare(exchange=config.EXCHANGE_NAME, exchange_type="topic")
 
 result = channel.queue_declare("", exclusive=True)
 queue_name = result.method.queue
-channel.queue_bind(exchange=EXCHANGE_NAME, queue=queue_name, routing_key=ROUTING_KEY)
+channel.queue_bind(
+    exchange=config.EXCHANGE_NAME, queue=queue_name, routing_key=ROUTING_KEY
+)
 channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
 
 print(" [*] Waiting for work. To exit press CTRL+C")
