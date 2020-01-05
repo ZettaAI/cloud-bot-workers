211fb56c920fc48c946127319a7c96846e2f5ef3
Akhilesh Halageri <akhileshhalageri@gmail.com>
2020-01-05 02:29:36 +0000

    help intro

--- workers/help.py
+++ workers/help.py
@@ -1,10 +1,11 @@
 """
 Display help for avaiable commands.
 """
-from typing import Union
-from typing import Iterable
 from json import dumps
 from json import loads
+from typing import Union
+from typing import Iterable
+from collections import OrderedDict
 
 
 from click import Group
@@ -20,7 +21,18 @@ from .slack import post_to_channel
 from .gcloud import cmd_grp as gcloud_grp
 
 ROUTING_KEY = "help.#"
-cmd_grps = {"gcloud": gcloud_grp}
+cmd_grps = OrderedDict()
+
+# insert alphabetically
+cmd_grps["gcloud"] = gcloud_grp
+
+
+def _get_help_msg():
+    msg_cmds = "\n".join(f"{cmd}" for cmd in cmd_grps.keys())
+    msg = "Following is a list of commands currently available.\n"
+    msg += f"```{msg_cmds}```"
+    msg += "\nType `help <command>` for more information about the `command`"
+    return msg
 
 
 def _get_nested_command(grp: Group, names: Iterable[str]) -> Union[Group, Command]:
@@ -32,21 +44,22 @@ def _get_nested_command(grp: Group, names: Iterable[str]) -> Union[Group, Comman
 
 
 def callback(ch, method, properties, body):
-    request_body = loads(body)
-    event = request_body["event"]
-
-    cmds = request_body["event"]["text"].split()[1:]
+    event = loads(body)["event"]
+    cmds = event["text"].split()[1:]
     assert cmds[0] == "help"
 
-    root_grp_or_cmd = cmd_grps[cmds[1]]
-    nested_grp_or_cmd = (
-        root_grp_or_cmd
-        if len(cmds) == 2
-        else _get_nested_command(root_grp_or_cmd, cmds[2:])
-    )
-
-    ctx = Context(nested_grp_or_cmd, info_name=" ".join(cmds[1:]))
-    r = post_to_channel(nested_grp_or_cmd.get_help(ctx), event["channel"])
+    if len(cmds) == 1:
+        msg = _get_help_msg()
+    else:
+        root_grp_or_cmd = cmd_grps[cmds[1]]
+        nested_grp_or_cmd = (
+            root_grp_or_cmd
+            if len(cmds) == 2
+            else _get_nested_command(root_grp_or_cmd, cmds[2:])
+        )
+        ctx = Context(nested_grp_or_cmd, info_name=" ".join(cmds[1:]))
+        msg = f"```{nested_grp_or_cmd.get_help(ctx)}```"
+    r = post_to_channel(msg, event["channel"])
     assert r.status_code == codes.ok  # pylint: disable=no-member
 
 
