d3b8e3d7c001fe00a8771f31125697c923116d1b
Akhilesh Halageri <akhileshhalageri@gmail.com>
2020-01-05 05:04:09 +0000

    try catch for creating bucket

--- workers/gcloud/buckets.py
+++ workers/gcloud/buckets.py
@@ -7,8 +7,11 @@ from google.cloud import storage
 
 def create(bucket_name):
     storage_client = storage.Client()
-    bucket = storage_client.create_bucket(bucket_name)
-    return f"{bucket.name} created."
+    try:
+        bucket = storage_client.create_bucket(bucket_name)
+        return f"`{bucket.name}` created."
+    except Exception as err:
+        return str(err)
 
 
 def lookup(bucket_name):
