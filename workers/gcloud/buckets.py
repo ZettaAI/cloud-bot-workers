012116e007472fac31de0b24a180f35c7d224002
Akhilesh Halageri <akhileshhalageri@gmail.com>
2020-01-12 01:54:53 +0000

    view iam policy for a bucket

--- workers/gcloud/buckets.py
+++ workers/gcloud/buckets.py
@@ -28,3 +28,16 @@ def lookup(ctx, *args, **kwargs):
     if not bucket:
         return f"Bucket `{bucket.name}` does not exist."
     return f"Bucket `{bucket.name}` exists."
+
+
+@bucket.command("iam", help="View IAM Policy for a bucket.", add_help_option=False)
+@click.option(
+    "--name", "-n", type=str, required=True, nargs=1, help="Name of the bucket."
+)
+@click.pass_context
+def iam(ctx, *args, **kwargs):
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(kwargs["name"])
+    policy = bucket.get_iam_policy()
+    msg = "\n".join(f"Role: {k}, members: {v}" for k, v in policy.items())
+    return f"```{msg}```"
