1cefa21cb35306521207575ad07d129a16c9f2a3
Akhilesh Halageri <akhileshhalageri@gmail.com>
2020-01-12 17:07:54 +0000

    register service account commands

--- workers/gcloud/__init__.py
+++ workers/gcloud/__init__.py
@@ -6,10 +6,12 @@ from click import Group
 from click import Context
 
 from .buckets import bucket as bucket_grp
+from .service_accounts import service_accounts as sa_grp
 
 ROUTING_KEY = "gcloud.#"
 
 
 cmd_grp = Group("gcloud", add_help_option=False)
 cmd_grp.add_command(bucket_grp)
+cmd_grp.add_command(sa_grp)
 
