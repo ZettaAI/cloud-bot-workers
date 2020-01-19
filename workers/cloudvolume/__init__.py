9f2a7ce38e359135a4179cf03d5572107f6d8a9a
Akhilesh Halageri <akhileshhalageri@gmail.com>
2020-01-19 04:16:23 +0000

    colud-volume commands

--- workers/cloudvolume/__init__.py
+++ workers/cloudvolume/__init__.py
@@ -1,6 +1,26 @@
 """
 Workers for commands that involve usage of CloudVolume.
-CloudVolume supports multiple protocols.
+CloudVolume supports multiple storage protocols.
 
 https://github.com/seung-lab/cloud-volume
 """
+
+import os
+
+import click
+
+from .cmds import storage as storage_grp
+
+ROUTING_KEY = "cv.#"
+
+
+@click.group(
+    "cv", help="Type help cv <subcommand> for information.", add_help_option=False,
+)
+@click.pass_context
+def cv(ctx, *args, **kwargs):
+    """Group for cloudvolume commands."""
+
+
+cv.add_command(storage_grp)
+
---
+++ workers/cloudvolume/cmds.py
@@ -0,0 +1,61 @@
+import os
+
+import click
+from cloudvolume import Storage
+
+
+@click.group(
+    "storage",
+    help="Subset of functionality supported by CloudVolume Storage.",
+    add_help_option=False,
+)
+@click.option(
+    "--n-threads",
+    "-n",
+    type=str,
+    required=False,
+    nargs=1,
+    help="Number of threads to perform the operation.",
+    default=lambda: os.environ["CV_STORAGE_THREADS"],
+    show_default=os.environ["CV_STORAGE_THREADS"],
+)
+@click.pass_context
+def storage(ctx, *args, **kwargs):
+    """Group for Storage commands."""
+    ctx.obj["n_threads"] = kwargs["n_threads"]
+
+
+@storage.command(
+    "copy",
+    help="Copy from source to destination. Paths must be protocols supported by CloudVolume.",
+    add_help_option=False,
+)
+@click.argument("src_path", type=str, required=True)
+@click.argument("dst_path", type=str, required=True)
+@click.pass_context
+def copy(ctx, *args, **kwargs):
+    with Storage(kwargs["src_path"], n_threads=ctx.obj["n_threads"],) as src, Storage(
+        kwargs["dst_path"], n_threads=ctx.obj["n_threads"],
+    ) as dst:
+        dst.put_files(src.get_files(src.list_files()))  # pylint: disable=no-member
+    return f"Copied files from `{kwargs['src_path']}` to `{kwargs['dst_path']}`"
+
+
+@storage.command(
+    "move",
+    help="Move from source to destination. "
+    "Paths must be protocols supported by CloudVolume."
+    "Warning: This will delete files in SRC_PATH.",
+    add_help_option=False,
+)
+@click.argument("src_path", type=str, required=True)
+@click.argument("dst_path", type=str, required=True)
+@click.pass_context
+def move(ctx, *args, **kwargs):
+    with Storage(kwargs["src_path"], n_threads=ctx.obj["n_threads"],) as src, Storage(
+        kwargs["dst_path"], n_threads=ctx.obj["n_threads"],
+    ) as dst:
+        files = src.list_files()
+        dst.put_files(src.get_files(files))  # pylint: disable=no-member
+        src.delete_files(files)
+    return f"Moved files from `{kwargs['src_path']}` to `{kwargs['dst_path']}`"
